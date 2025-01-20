87ea5ef4eb0fe54d4a8dd469841773486a5f7468
aline <aline.s.saraiva2002@gmail.com>
2025-01-20 19:41:03 -0300

    Excercício do Teste do Triângulo

---
+++ Ex008/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidade;
+            Console.WriteLine("Velocidade do veiculo");
+            Convert.ToDouble         }
+    }
+}
